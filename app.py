8c163a5d1d205e3161cb1e206112d4341e5a2727
suthamk <suthambk@gmail.com>
2023-11-17 18:16:47 +0800

    modified:   app.py 	modified:   templates/tabs.html

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import subprocess
 import csv
 from datetime import datetime
 import os
+import io
+from openpyxl import Workbook,load_workbook
 
 
 app = Flask(__name__)
@@ -21,6 +23,34 @@ def get_last_modified_datetime(file_path):
         return None
     
 last_modified_datetime = get_last_modified_datetime('/home/rancid/test/router.db')
+
+def create_template(data):
+    template_file_path = '/home/rancid/test/template_file.xlsx'
+    template_wb = load_workbook(template_file_path)
+    sheet = template_wb.active
+
+    additional_string = "Unknown,Unknown,Department#Department#TRANSMISSION|Device-Type#Device-Type#TRS#Huawei|Device-Location#Device-Location|Device-Group#Device-Group#TRS#TRS_HUAWEI_MODEL|IPSEC#Is IPSEC Device|Device Type#All Device Types|Location#All Locations,,,,,,,,,,,,,,,,,,,,,,,,,,ENABLE_USING_COA,,,,,,,,CisC0IS3,OFF,Huawei,1700,FALSE,2083,,,ise-pan-mnt-ts01.starhubsg.sh.inc,,"
+    additional_values = additional_string.split(',')
+
+    for item in data:
+        # Convert the OrderedDict to a list
+        item.insert(2, item[1])
+        if "ATN950D" in item[0]:
+            item[1] = "Huawei, ATN950D"
+        elif "ATN910CG" in item[0]:
+            item[1] = "Huawei, ATN910CG"
+
+        item[2] = item[2] + "/32"
+        item.extend(additional_values)
+        sheet.append(item)
+
+    return template_wb
+
+def read_data_from_file(file_path):
+    with open(file_path, 'r') as file:
+        data = [line.strip().split(',') for line in file]
+    return data
+
 
 @app.route('/', methods=['GET', 'POST'])
 def display_data():
@@ -96,5 +126,41 @@ def download_router_db():
     router_db_file = '/home/rancid/test/router.db'
     return send_file(router_db_file, as_attachment=True)
 
+@app.route('/generate_template', methods=['POST'])
+def generate_template():
+    input_text = request.form['ISE_Node']
+
+    with open('input.txt', 'w') as file:
+        file.write(input_text)
+
+    script_path = '/home/rancid/test/app_ise.sh'
+    subprocess.run(['bash', script_path])
+
+    # Read data from a file (in this case, a CSV file)
+    output_file_path = '/home/rancid/test/output.txt'
+    data = read_data_from_file(output_file_path)
+
+    # Create the Excel template with data from the file
+    wb = create_template(data)
+
+    # Specify the path for the output file
+    output_file_path = '/home/rancid/test/ise_output_file.xlsx'
+
+    # Save the modified workbook to the output file
+    wb.save(output_file_path)
+
+    # Load the saved workbook into a BytesIO object for downloading
+    excel_file = io.BytesIO()
+    with open(output_file_path, 'rb') as file:
+        excel_file.write(file.read())
+
+    excel_file.seek(0)
+
+    return send_file(
+        excel_file,
+        mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        as_attachment=True,
+        download_name='ise_output_file.xlsx'
+    )
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
